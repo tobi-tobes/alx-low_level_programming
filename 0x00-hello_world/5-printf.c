862dad3a1306b97cd64b35eceb16a71133e82ed4
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-08 08:11:11 -0800

    Task 5 correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	/* Prints given string to console */
-	printf("with proper grammar, but the outcome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
