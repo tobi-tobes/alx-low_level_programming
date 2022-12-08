2f7901fdc1d3fe4a2fc3352fe0bba81db59e5ae7
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-08 11:10:28 -0800

    Task 7&8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
+}
