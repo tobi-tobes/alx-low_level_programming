3d1c4cd9c29316f1e51b410b6a46d42505dc7f6d
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-08 04:51:27 -0800

    Task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/* This program prints out a string with the puts() function */
+
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(str1);
-	return 0;
+	return (0);
 }
