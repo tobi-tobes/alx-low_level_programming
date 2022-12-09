21e9a1eba3846506c5cf2e37bfbce3e61545927f
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-09 08:13:38 -0800

    Task 1 correction, separated variable declaration and assignment

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	/* check last digit of */
-	int last_digit = n % 10;
+	int last_digit;
+	
+	last_digit = n % 10;
 
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
