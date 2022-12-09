c80bd6bf19ed82216ffb8e38535807adf4ae63ab
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-09 14:04:22 -0800

    Task 8 correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,15 @@
 int main(void)
 {
 	int n;
+	char c;
 
-	for (n = 0x0; n <= 0xf; n++)
+	for (n = 0x0; n <= 0x9; n++)
 	{
 		putchar(n + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
