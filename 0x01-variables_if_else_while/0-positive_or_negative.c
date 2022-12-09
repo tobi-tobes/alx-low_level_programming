e90daeb9e3b934f24f4163ec217a3c30f0e810f1
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-09 07:51:28 -0800

    Task 0 of C variables removed curly braces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* use if...else statement to check n */
-	if (n > 0)
-	{
+	if (n > 0
 		printf("%d is positive\n", n);
-	} else if (n == 0) {
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	} else {
+	else
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
