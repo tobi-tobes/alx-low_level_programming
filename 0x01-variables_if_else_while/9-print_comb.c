79238f7638d02419bef9f46326561d60cd4f4053
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-09 14:22:15 -0800

    Task 9 correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	for (n = 0; n <= 9; n++)
 	{
 		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (n == 9)
+			continue;
+		else
+			putchar(',');
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
