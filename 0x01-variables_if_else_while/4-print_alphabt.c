774e49aef7cbe19225c3725ff068944c7bb1d306
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-09 12:25:33 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* declare variable for counting */
+	char c;
+
+	/* use for loop and if-else statement to print alphabet */
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == e || c == q)
+			continue;
+		else
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
