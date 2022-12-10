bd46b6133acf4cf5f9b6b239e9e63c52666eb82d
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-09 16:31:01 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,5 +7,35 @@
  */
 int main(void)
 {
+	int i;
+	int h;
+	int t;
+	int u;
+
+	h = (i / 100) % 10;
+	t = (i / 10) % 10;
+	u = i % 10;
+
+	for (i = 0; i < 790; i++)
+	{
+		if (u == t || h == u || h == t)
+			continue;
+		else if (h > t || h > u || t > u)
+			continue;
+		else
+		{
+			putchar(h);
+			putchar(t);
+			putchar(u);
+			if (i == 789)
+				continue;
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
