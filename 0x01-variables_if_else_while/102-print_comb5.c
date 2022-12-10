6ffa97436984f5a7cd62da064e2196d42155ae00
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-09 18:50:28 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,5 +7,47 @@
  */
 int main(void)
 {
+	/* declare int variables for for loop counter */
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if ((j / 10) == 0 && (j / 10) == (j % 10))
+				continue;
+			else if ((i / 10) == (j / 10) &&
+				 (i % 10) == (j % 10))
+				continue;
+			else if ((i / 10) == (j / 10) &&
+				 (i % 10) > (j % 10))
+				continue;
+			else if ((i / 10) > (j / 10) &&
+				 (i % 10) > (j % 10))
+				continue;
+			else if ((i / 10) > (j / 10) &&
+				 (i % 10) == (j % 10))
+				continue;
+			else if ((i / 10) > (j / 10))
+				continue;
+			else
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (j == 99 && i == 98)
+					continue;
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
