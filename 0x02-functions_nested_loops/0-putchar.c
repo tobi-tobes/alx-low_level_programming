366a8d31aa93786306d04460c99be6b3ab2f3617
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-13 09:30:41 -0800

    Task 0 correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * main - check the code
@@ -7,14 +8,6 @@
  */
 int main(void)
 {
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
-	_putchar(10);
+	write(1, "_putchar\n", 9);
 	return (0);
 }
