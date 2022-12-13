af9c6589eb4e121979fef77c75d7fe736357a6a9
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-13 14:17:43 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+/**
+ * print_sign - prints +, 0, or - based on the number
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * Return 0 or -1 when zero or negative respectively.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		write(1, "+", 1);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		write(1, "0", 1);
+		return (0);
+	}
+	else
+	{
+		write(1, "-", 1);
+		return (-1);
+	}
+}
