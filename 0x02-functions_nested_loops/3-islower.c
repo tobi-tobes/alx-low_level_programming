5ae2f6a9069a606095126bae83d12ebe4224288b
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-13 12:49:28 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <unistd>
+#include <ctype.h>
+
+/**
+ * _islower(int c): returns 1 if lowercase and 0 otherwise
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
