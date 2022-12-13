3f4549a1cf80bdc0e3a39a413604cf52f57ec923
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-13 14:42:00 -0800

    Task 6 correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
-#include <stdlib.h>>
+#include <stdlib.h>
 
 /**
  * _abs - computes the absolute value
- * @int: The character to print
+ * @n: The character to print
  *
  * Return: On success, any number.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _abs(int)
+int _abs(int n)
 {
-	return (abs(int));
+	return (abs(n));
 }
