28504fbd6bb85f1de279b84319739b1b38bdbc6f
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-13 17:53:35 -0800

    Task 9 correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,24 +8,21 @@
  */
 void times_table(void)
 {
-	int i, j, m, m_casted, m_upper, m_lower;
+	int i, j, m,  m_upper, m_lower;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			m = (i * j);
-			m_casted = m + '0';
 			m_upper = (m / 10) + '0';
 			m_lower = (m % 10) + '0';
 
-			if (m > 9)
-			{
+			if ((m / 10) > 0)
 				write(1, &m_upper, 1);
-				write(1, &m_lower, 1);
-			}
 			else
-				write(1, &m_casted, 1);
+				write(1, " ", 1);
+			write(1, &m_lower, 1);
 			if (j == 9)
 				continue;
 			else
