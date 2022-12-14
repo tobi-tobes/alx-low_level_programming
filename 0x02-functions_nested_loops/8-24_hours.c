e3d1444bb12a015c74cfdd026ed323875b0eb612
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-13 16:50:25 -0800

    Task 8 correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,17 +11,26 @@ void jack_bauer(void)
 {
 	int h;
 	int m;
+	int h1;
+	int h2;
+	int m1;
+	int m2;
 
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			write(1, (h / 10) + '0', 1);
-			write(1, (h % 10) + '0', 1);
-			write(1, ':', 1);
-			write(1, (m / 10) + '0', 1);
-			write(1, (m % 10) + '0', 1);
-			write(1, '\n', 1);
+			h1 = (h / 10) + '0';
+			h2 = (h % 10) + '0';
+			m1 = (m / 10) + '0';
+			m2 = (m % 10) + '0';
+
+			write(1, &h1, 1);
+			write(1, &h2, 1);
+			write(1, ":", 1);
+			write(1, &m1, 1);
+			write(1, &m2, 1);
+			write(1, "\n", 1);
 		}
 	}
 }
