3c16febf2a690a0aeaadde93885f69a93d0c864d
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-15 04:06:47 -0800

    Tasks 0 to 2

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: The character to check
+ *
+ * Return: 1 if uppercase, 0 if not
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two given digits
+ * @a: The first multiplier
+ * @b: The second multiplier
+ *
+ * Return: the product of a and b
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
