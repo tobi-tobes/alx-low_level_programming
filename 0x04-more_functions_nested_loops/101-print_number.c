251ae69084c17bfabe2d1bda1a6a7281056d5f74
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-15 12:01:49 -0800

    Task 12 correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,6 +17,17 @@ void print_number(int n)
 		{
 			_putchar((n / 10) + '0');
 		}
+		else if (abs(n) > 99)
+		{
+			_putchar((n / 100) + '0');
+			_putchar(((n / 10) % 10) + '0');
+		}
+		else if (abs(n) > 999)
+		{
+			_putchar((n / 1000) + '0');
+			_putchar(((n / 100) % 10) + '0');
+			_putchar(((n / 10) % 10) + '0');
+		}
 	}
 	else if (n > 9)
 	{
