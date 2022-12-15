1bd3c5fc28ec3189d7ea55e36d68cd6b69907663
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-15 04:50:08 -0800

    Task 4 correction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9 except 2 and 4, followed by new line
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
  *
  * Return: no value
  * On error, -1 is returned, and errno is set appropriately.
@@ -14,7 +14,7 @@ void print_most_numbers(void)
 
 	while (c <= '9')
 	{
-		if (c != '2' || c != '4')
+		if (c != '2' && c != '4')
 		{
 			_putchar(c);
 		}
