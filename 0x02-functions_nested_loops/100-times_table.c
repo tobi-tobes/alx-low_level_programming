b2211f89024ba0d73bb3b4a652ec0108acc65fc7
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-14 16:22:44 -0800

    Task 12 correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,45 @@ void print_times_table(int n)
 {
 	if (n >= 0 && n <= 15)
 	{
-		times_table_checker(n);
+		int i, j, m;
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				m = (i * j);
+				if (m > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((m / 100) + '0');
+					_putchar(((m / 10) % 10) + '0');
+					_putchar((m % 10) + '0');
+				}
+				else if (m > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((m / 10) % 10) + '0');
+					_putchar((m % 10) + '0');
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(m + '0');
+				}
+			}
+			if (i != n)
+			{
+				_putchar('\n');
+			}
+		}
+		_putchar('\n');
 	}
 }
