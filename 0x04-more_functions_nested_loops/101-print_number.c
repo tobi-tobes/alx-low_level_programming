5344244e3f1731d288692535af416f81fed4f9d6
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-15 15:06:02 -0800

    Task 12 correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,39 +10,23 @@
  */
 void print_number(int n)
 {
+	int num, size;
+
 	if (n < 0)
 	{
+		num = abs(n);
 		_putchar('-');
-		if (abs(n) > 9)
-		{
-			_putchar((n / 10) + '0');
-		}
-		else if (abs(n) > 99)
-		{
-			_putchar((n / 100) + '0');
-			_putchar(((n / 10) % 10) + '0');
-		}
-		else if (abs(n) > 999)
-		{
-			_putchar((n / 1000) + '0');
-			_putchar(((n / 100) % 10) + '0');
-			_putchar(((n / 10) % 10) + '0');
-		}
 	}
-	else if (n > 9)
+	else
 	{
-		_putchar((n / 10) + '0');
+		num = n;
 	}
-	else if (n > 99)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-	}
-	else if (n > 999)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-	}
-	_putchar((n % 10) + '0');
+
+	size = 1000000000;
+
+	do {
+		if (size <= num || size == 1)
+			_putchar((num / size) % 10 + '0');
+		size = size / 10;
+	} while (size != 0);
 }
