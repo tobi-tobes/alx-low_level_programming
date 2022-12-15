8e117d5c04ade95a8e59c34bb47ea1d3729b8c61
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-14 16:06:23 -0800

    Task 12 correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,47 +11,6 @@ void print_times_table(int n)
 {
 	if (n >= 0 && n <= 15)
 	{
-		int i, j, m;
-
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				m = (i * j);
-
-				if (m > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((m / 100) + '0');
-					_putchar(((m / 10) % 10) + '0');
-					_putchar((m % 10) + '0');
-				}
-				else if (m > 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(((m / 10) % 10) + '0');
-					_putchar((m % 10) + '0');
-				}
-				else
-				{
-					if (j != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(m + '0');
-				}
-			}
-			if (i != n)
-			{
-				_putchar('\n');
-			}
-		}
-		_putchar('\n');
+		times_table_checker(n);
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,3 +20,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void times_table_checker(int n);
