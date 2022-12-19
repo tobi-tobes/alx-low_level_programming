3c832d7c1e060f6bdbd786f5fb40997688eb30e3
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-19 13:19:14 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverses a string
+ *@s: string to be reversed
+ *
+ * Return: no value
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	i = strlen(s);
+
+	for (i -= 1; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+}
