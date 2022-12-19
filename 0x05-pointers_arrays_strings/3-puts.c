71c1cc33461329ef3d8f9e15b75f8efdf349ec34
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-19 12:34:33 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string to be printed out
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
