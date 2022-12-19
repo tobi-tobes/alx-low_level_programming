9d326af561f9944cdc96e32cf478902539673e78
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-19 11:33:13 -0800

    Task 1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
