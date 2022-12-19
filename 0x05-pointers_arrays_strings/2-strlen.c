6d78deea5c24c765bab615c2aeacd8abf28d6f38
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-19 12:25:14 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length will be checked
+ *
+ * Return: the length of the given string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
