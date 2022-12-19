d7ed5569793e9217348dac73aad35113281aa335
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-19 15:19:50 -0800

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ *
+ * Return: converted number if successful, 0 if error
+ */
+int _atoi(char *s)
+{
+	int p, i, sign, length;
+	unsigned int num;
+
+	p = 0;
+	sign = 1;
+	num = 0;
+	length = 0;
+
+	while (s[p] != '\0')
+	{
+		length++;
+		p++;
+	}
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+			num = (num * 10) + (s[i] - '0');
+		else
+			break;
+	}
+	num *= sign;
+	return (num);
+}
