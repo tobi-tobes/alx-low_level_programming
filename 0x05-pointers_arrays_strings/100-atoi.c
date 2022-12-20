1dc7df738176f7e63b0b4fe267f702cbba238bf5
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-20 13:38:17 -0800

    Task 10 correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,7 +28,12 @@ int _atoi(char *s)
 			continue;
 		}
 		if (s[i] >= '0' && s[i] <= '9')
+		{
 			num = (num * 10) + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			else
+				continue;
 		else
 			continue;
 	}
