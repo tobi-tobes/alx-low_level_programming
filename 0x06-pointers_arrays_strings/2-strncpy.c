45612261219790fbe815ff888cd1d0c739330024
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-21 13:13:10 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (src[i] == '\0')
-			dest[i] = '\0';
-		else
-			dest[i] = src[i];
+		dest[i] = src[i];
 	}
+
+	dest[n] = '\0';
+
 	return (dest);
 }
