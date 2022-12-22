f2247a617d9488135fee900d590009cb82beb296
Tobi Tijani <tobi_tijani@yahoo.com>
2022-12-21 23:23:18 -0800

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @c: pointer to the string to be encoded
+ *
+ * Return: encoded string.
+ */
+char *rot13(char *c)
+{
+	int i;
+
+	for (i = 0; c[i]; i++)
+	{
+		while ((c[i] >= 'A' && c[i] <= 'Z') ||
+		       (c[i] >= 'a' && c[i] <= 'z'))
+		{
+			if ((c[i] >= 'N' && c[i] <= 'Z') ||
+			    (c[i] >= 'n' && c[i] <= 'z'))
+				{
+					c[i] = c[i] - 13;
+					break;
+				}
+			c[i] = c[i] + 13;
+			break;
+		}
+	}
+	return (c);
+}
