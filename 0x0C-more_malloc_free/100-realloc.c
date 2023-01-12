5c0d2e08850badcc788630fa76b7228a0ef9f7d6
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-12 06:46:11 -0800

    Task 100 correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,12 +30,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	np = malloc(new_size);
-       	i = 0;
+	i = 0;
+
 	while (i < new_size && i < old_size)
-		{
-			np[i] = ((char *) ptr)[i];
-			i++;
-		}
+	{
+		np[i] = ((char *) ptr)[i];
+		i++;
+	}
 	free(ptr);
 	return (np);
 }
