1a023aa383739a33e7b01a4aa9f0936690532799
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-18 07:14:06 -0800

    Corrections, added necessary header files to function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef _HEADER_
 #define _HEADER_
+#include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
