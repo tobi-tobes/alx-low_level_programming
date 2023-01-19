97ba1738f1798f8eb1dca9681e7f12c33ab420cb
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-19 08:34:33 -0800

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	int sum = 0;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (n != 0)
 	{
 		va_start(ap, n);
-		for (; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			sum += va_arg(ap, int);
 		}
