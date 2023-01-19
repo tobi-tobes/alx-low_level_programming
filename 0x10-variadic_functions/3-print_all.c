0f5665020a37d66fbc24d2b89686b2e299d86adc
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-19 07:26:37 -0800

    Task 3 correction: added separator variable to remove need for extra if statement

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,52 +5,56 @@
 /**
  * print_char - prints a char
  * @argp: pointer to current variable argument
+ * @separator: delimiter between parameters
  *
  * Return: Nothing
  */
-void print_char(va_list argp)
+void print_char(va_list argp, char *separator)
 {
-	printf("%c", va_arg(argp, int));
+	printf("%s%c", separator, va_arg(argp, int));
 }
 
 /**
  * print_int - prints an integer
  * @argp: pointer to current variable argument
+ * @separator: delimiter between parameters
  *
  * Return: Nothing
  */
-void print_int(va_list argp)
+void print_int(va_list argp, char *separator)
 {
-	printf("%d", va_arg(argp, int));
+	printf("%s%d", separator, va_arg(argp, int));
 }
 
 /**
  * print_float - prints a float
  * @argp: pointer to current variable argument
+ * @separator: delimiter between parameters
  *
  * Return: Nothing
  */
-void print_float(va_list argp)
+void print_float(va_list argp, char *separator)
 {
-	printf("%f", va_arg(argp, double));
+	printf("%s%f", separator, va_arg(argp, double));
 }
 
 /**
  * print_string - prints a string
  * @argp: pointer to current variable argument
+ * @separator: delimiter between parameters
  *
  * Return: Nothing
  */
-void print_string(va_list argp)
+void print_string(va_list argp, char *separator)
 {
 	char *string = va_arg(argp, char *);
 
 	if (string == NULL)
 	{
-		printf("(nil)");
+		printf("(nil)%s", separator);
 		return;
 	}
-	printf("%s", string);
+	printf("%s%s", separator, string);
 }
 
 /**
@@ -69,20 +73,18 @@ void print_all(const char * const format, ...)
 	};
 	int i = 0, j;
 	va_list ap;
+	char *separator = "";
 
 	va_start(ap, format);
-
 	while (format != NULL && format[i])
 	{
 		j = 0;
-
 		while (j < 4)
 		{
 			if (format[i] == *formats[j].type)
 			{
-				formats[j].p(ap);
-				if (format[i + 1])
-					printf(", ");
+				formats[j].p(ap, separator);
+				separator = ", ";
 			}
 			j++;
 		}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,10 +8,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list argp);
-void print_int(va_list argp);
-void print_float(va_list argp);
-void print_string(va_list argp);
+void print_char(va_list argp, char *separator);
+void print_int(va_list argp, char *separator);
+void print_float(va_list argp, char *separator);
+void print_string(va_list argp, char *separator);
 int _putchar(char c);
 
 /**
@@ -22,7 +22,7 @@ int _putchar(char c);
  */
 typedef struct format_code {
 	char *type;
-	void (*p)(va_list argp);
+	void (*p)(va_list argp, char *separator);
 } f_t;
 
 #endif
