bca084f693eb0bf9107b7ba76bf9eb9dbe47c8d9
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-19 08:15:37 -0800

    Tasks 1 and 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,7 +28,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if ((i != n - 1) && (separator != NULL))
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(ap);
 	}
 	printf("\n");
