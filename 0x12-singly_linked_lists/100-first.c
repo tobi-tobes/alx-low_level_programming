577d855dbbb22727f610519b71b39e30c07c275b
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-26 06:06:42 -0800

    Added void in Task 5 and moved struct upwards in lists.h

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
 
 /**
  * before_main - executes before main
  *
  * Return: nothing
  */
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,15 +1,9 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _LINKED_LIST
+#define _LINKED_LIST
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int _putchar(c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+#include <unistd.h>
 
 /**
  * struct list_s - singly linked list
@@ -25,5 +19,12 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchat(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
