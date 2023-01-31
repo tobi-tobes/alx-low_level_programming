4f9f2b259558f8815d874df5e44e7850752329d4
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-31 04:06:28 -0800

    Tasks 0-2 corrected struct in lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,19 +4,18 @@
 #include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *
  */
-typedef struct list_s
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
