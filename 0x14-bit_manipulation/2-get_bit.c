2eab2a25a5820a6377782b6fda4c687487ae8eb7
Tobi Tijani <tobi_tijani@yahoo.com>
2023-02-02 08:55:30 -0800

    Task 2: added condition for index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int counter = 0;
 
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+
 	while (n > 0)
 	{
 		if (counter == index)
