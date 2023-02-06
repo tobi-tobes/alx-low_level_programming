e5e3893b0ee45a64bcb8be2c2577f23d02fbffff
Tobi Tijani <tobi_tijani@yahoo.com>
2023-02-06 14:02:38 -0800

    Split main function into sub-function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,60 +1,72 @@
 #include "main.h"
 
 /**
+ * copier - copies the content of one file to another
+ * @file1: source file
+ * @file2: destination file
+ *
+ * Return: nothing on success or 98, 99, or 100 if error
+ */
+void copier(char *file1, char *file2)
+{
+	int fd1, fd2;
+	ssize_t bytes_read;
+	char buffer[1024];
+
+	fd1 = open(file1, O_RDONLY);
+
+	if (fd1 < 0)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", file1);
+		exit(98);
+	}
+
+	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while ((bytes_read = read(fd1, buffer, sizeof(buffer))) > 0)
+	{
+		if (write(fd2, buffer, bytes_read) != bytes_read || fd2 < 0)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file2);
+			exit(99);
+		}
+	}
+	if (bytes_read < 0)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", file1);
+		exit(98);
+	}
+	if (close(fd1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd2);
+		exit(100);
+	}
+}
+
+/**
  * main - copies the content of a file to another file.
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 on success or 97, 98, 99, or 100 if error
+ * Return: 0 on success or 97 on error
  */
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		int fd1, fd2;
-		ssize_t bytes_read;
-		char buffer[1024];
-
-		fd1 = open(argv[1], O_RDONLY, 0);
-
-		if (fd1 < 0)
+		if (argv[1] != NULL && argv[2] != NULL)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			exit(98);
+			copier(argv[1], argv[2]);
+			return (0);
 		}
-
-		fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-		while ((bytes_read = read(fd1, buffer, sizeof(buffer))) > 0)
-		{
-			if (write(fd2, buffer, bytes_read) != bytes_read ||
-			    fd2 < 0)
-			{
-				dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-		}
-		if (bytes_read < 0)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		if (close(fd1) < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd%d\n",
-				fd1);
-			exit(100);
-		}
-		if (close(fd2) < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd%d\n",
-				fd2);
-			exit(100);
-		}
-		return (0);
 	}
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 	exit(97);
