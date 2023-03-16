07223498658a1b3f2ef041a2479b46c6bb19fa1f
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-16 15:20:16 -0700

    Task 7 correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL || (*h) == NULL)
 		return (NULL);
-
 	if ((*h)->prev != NULL)
 	{
 		while ((*h)->prev != NULL)
@@ -29,18 +28,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (counter < idx)
 		return (NULL);
-
-	if (temp->prev == NULL)
-		return (add_dnodeint(h, n));
-	else if (temp->next == NULL && temp->prev != NULL)
-		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = temp;
-	new_node->prev = temp->prev;
-	temp->prev->next = new_node;
-	temp->prev = new_node;
+	if (temp->prev == NULL)
+	{
+		new_node->prev = NULL;
+		temp->prev = new_node;
+		(*h) = new_node;
+	}
+	else
+	{
+		new_node->prev = temp->prev;
+		temp->prev->next = new_node;
+		temp->prev = new_node;
+	}
 	return (new_node);
 }
