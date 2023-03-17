1291139975761b039393d791e71d87ac5f293439
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-17 14:18:45 -0700

    Task 7: added check for empty list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
+
+	if ((*h) == NULL && idx == 0)
+		return (add_dnodeint(h, n));
 	if ((*h)->prev != NULL)
 	{
 		while ((*h)->prev != NULL)
@@ -28,23 +31,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (counter < idx)
 		return (NULL);
-	if ((*h) == NULL && idx == 0)
+
+	if (temp->prev == NULL)
 		return (add_dnodeint(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = temp;
-	if (temp->prev == NULL)
-	{
-		new_node->prev = NULL;
-		(*h) = temp->prev = new_node;
-	}
-	else
-	{
-		new_node->prev = temp->prev;
-		temp->prev->next = new_node;
-		temp->prev = new_node;
-	}
+	new_node->prev = temp->prev;
+	temp->prev->next = new_node;
+	temp->prev = new_node;
 	return (new_node);
 }
