347dccfbefb497153738bd83ff0d1ac38cced0dc
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-17 12:46:14 -0700

    Task 7: added check for empty list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (counter < idx)
 		return (NULL);
-	if ((*h) == NULL)
+	if ((*h) == NULL && idx == 0)
 		return (add_dnodeint(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
