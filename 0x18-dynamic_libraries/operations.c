49f3f489d509ff2e58d40a67b67e1f66cf816b74
Tobi Tijani <tobi_tijani@yahoo.com>
2023-03-20 06:17:36 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adds two variables
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two variables
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two variables
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two variables
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns remainder of division of two variables
+ * @a: variable a
+ * @b: variable b
+ *
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
