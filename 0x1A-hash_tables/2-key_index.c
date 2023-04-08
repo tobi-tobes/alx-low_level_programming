c08dc4ca6658eab64ea7149ae6d606cf12aa928f
Tobi Tijani <tobi_tijani@yahoo.com>
2023-04-07 17:45:04 -0700

    Task 0 and 2 corrections

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,13 +5,13 @@
  * @key: key to be hashed
  * @size: size of the array of the hash table
  *
- * Return: index where key/value pair should be stored or size + 1 if key is invalid
+ * Return: index where key/value pair should be stored or size + 1 on failure
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
 
-	if (strlen(key) == 0 or size == 0)
+	if (size == 0)
 		return (size + 1);
 
 	index = hash_djb2(key) % size;
